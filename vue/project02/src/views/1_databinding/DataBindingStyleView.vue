8329b38fccfb7123290e8f7df01efa1937039211
yeojinyoun <97152534+Yeojinyoun@users.noreply.github.com>
2022-03-03 20:03:43 +0900

    [ADD] vue databinding/event

---
+++ vue/project02/src/views/1_databinding/DataBindingStyleView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div :stlye="style1">스타일 바인딩 : 글씨는 red, 폰트크기는: 30px</div>
+    <div :stlye="style2">스타일 바인딩 : 글씨는 red, 폰트크기는: 30px</div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      style1: 'color:red; font-size:30px',
+      style2: {
+        color: 'red',
+        fontSize: '30px'
+      }
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
